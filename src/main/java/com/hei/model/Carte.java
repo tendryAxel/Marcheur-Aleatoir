d4cb7761bf6a4f40469e1cbd0e2e822299660ffa
tendryAxel <hei.tendry@gmail.com>
2024-06-17 10:57:25 +0300

    feat: class Lieu

--- src/main/java/com/hei/model/Carte.java
+++ src/main/java/com/hei/model/Carte.java
@@ -1,13 +1,22 @@
 package com.hei.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Carte {
     private final Marcheur marcheur;
+    private final Set<Lieu> lieux;
 
     public Carte(Marcheur marcheur) {
         this.marcheur = marcheur;
+        this.lieux = new HashSet<>();
     }
 
     public Marcheur getMarcheur() {
         return marcheur;
+    }
+
+    public Set<Lieu> getLieux() {
+        return lieux;
     }
 }
